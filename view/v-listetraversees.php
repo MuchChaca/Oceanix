8bc7b4312139bff4465b4e546b6c4d8faf893240
archy <chkmake@gmail.com>
2017-04-24 17:42:22 +0000

    Template of 'Traversees' done, all the admin links still need to be added git add .

--- view/v-listetraversees.php
+++ view/v-listetraversees.php
@@ -14,15 +14,17 @@
 	<div class="col-sm-4 portfolio-item">
 		<?php
 
-		echo "<div=\"\">";
+		echo "<div class=\"display-table\">";
 		echo "<table>
 		<thead>
 			<tr>
 				<th>Numéro</th>
 				<th>Heure</th>
-				<th colspan=\"1\">Bateau</th>
-				<th></th>
-			</tr>
+				<th colspan=\"2\">Bateau</th>";
+				if(Passerelle::logged()){
+					echo "<th class=\"admin\" colspan=\"3\">Action</th>";
+				}
+			echo"</tr>
 		</thead>";
 		echo "<tbody>";
 			foreach($traversees as $laTraversee){
@@ -30,8 +32,20 @@
 				<td>".$laTraversee->num()."</td>
 				<td>".$laTraversee->heure()."</td>
 				<td>".$laTraversee->bateau()->id()."</td>
-				<td><a href=?action=choixtraversees&numTrav=".$laTraversee->num().">Réserver</a></td>
-				</tr>";
+				<td><a href=?action=choixtraversees&numTrav=".$laTraversee->num().">Réserver</a></td>";
+				if(Passerelle::logged()):
+					echo
+					"<td><a href=\"index.php?action=\">
+						<i class=\"fa fa-eye\" aria-hidden=\"true\" alt=\"Voir\" title=\"Voir\"></i>
+					</a></td>
+					<td><a href=\"index.php?action=\">
+						<i class=\"fa fa-pencil-square-o\" aria-hidden=\"true\" alt=\"Modifier\" title=\"Modifier\"></i>
+					</a></td>
+					<td><a href=\"index.php?action=\">
+						<i class=\"fa fa-trash\" aria-hidden=\"true\" alt=\"Supprimer\" title=\"Supprimer\"></i>
+					</a></td>";
+				endif;
+				echo "</tr>";
 			}
 			echo"</tbody>
 		</table>";
